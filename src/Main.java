01778e92357223a13fac95973c426795a8436988
GiuliaMarsano <159422477+GiuliaMarsano@users.noreply.github.com>
2024-02-16 19:29:22 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Marsano Giulia");
 		System.out.println("Casto Samuele");
+		System.out.println("Padovano Samuele");
+		System.out.println("Panico Matteo");
+		System.out.println("Russo Benedetta");
 	}
 }
